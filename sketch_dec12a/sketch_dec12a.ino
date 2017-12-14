7e87383b22ba22d3ffcb57a5713625bd1aa61f01
mkfloyd <mfloyd@bancroftschool.org>
2017-12-14 13:47:12 -0500

    Add files via upload

--- sketch_dec12a/sketch_dec12a.ino
+++ sketch_dec12a/sketch_dec12a.ino
@@ -1,25 +1,51 @@
+int speed = 200;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);
-  delay(500);
+  delay(speed);
   digitalWrite(13, LOW);
-  delay(500);
 
-  pinMode(12, OUTPUT);
+
   digitalWrite(12, HIGH);
-  delay(500);
+  delay(speed);
   digitalWrite(12, LOW);
-  delay(500);
 
-  pinMode(11, OUTPUT);
+
   digitalWrite(11, HIGH);
-  delay(500);
+  delay(speed);
   digitalWrite(11, LOW);
-  delay(500);
+
+
+
+  digitalWrite(10, HIGH);
+  delay(speed);
+  digitalWrite(10, LOW);
+
+
+  digitalWrite(9, HIGH);
+  delay(speed);
+  digitalWrite(9, LOW);
+
+
+
+
+  digitalWrite(8, HIGH);
+  delay(speed);
+  digitalWrite(8, LOW);
+
+
+  
+
 }
